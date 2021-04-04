7ffaa07b7d4cb767af48721e2174356c2ba1cebf
rizqizulfian <rizqizulfian@gmail.com>
2021-04-05 02:22:26 +0700

    add constraint user table

---
+++ controllers/userscontroller.js
@@ -0,0 +1,8 @@
+const { User } = require('../models')
+
+class UsersController {
+    static signup(req, res, next) {
+        const { username, email, password } = req.body
+        
+    }
+}
--- models/user.js
+++ models/user.js
@@ -1,7 +1,9 @@
 'use strict';
+const bcrypt = require('../helpers/bcrypt')
 const {
   Model
 } = require('sequelize');
+const { hash } = require('bcrypt');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -14,10 +16,44 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   User.init({
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+    username: {
+      validate: {
+        type: DataTypes.STRING,
+        notEmpty: {
+          msg: 'username cant empty'
+        }
+      }
+    },
+    email: {
+      validate: {
+        type: DataTypes.STRING,
+        isEmail: {
+          msg: 'please insert valid email'
+        },
+        notEmpty: {
+          msg: 'username cant empty'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        password(value) {
+          if ( value.length < 6 ) {
+            throw 'Minimal password lenght is 6'
+          }
+        },
+        notEmpty : {
+          msg: 'password cant empty'
+        }
+      }
+    }
   }, {
+    hooks: {
+      beforeCreate: (user, options) => {
+        user.password = hashPassword(user.password)
+      }
+    },
     sequelize,
     modelName: 'User',
   });
