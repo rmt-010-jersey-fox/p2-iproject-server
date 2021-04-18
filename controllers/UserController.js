91c38cc8fc59151a0593a2af3d5c1686d6726a3d
farhantaufiks <farhantaufiks@gmail.com>
2021-04-18 19:04:46 +0700

    user register login

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,46 @@
+const { comparepass } = require('../helpers/bcrypt')
+const { generateToken } = require('../helpers/jwt')
+const { User } = require('../models')
+
 class UserController{
-  static register(req, res, next)
+  static register(req, res, next) {
+    let input = {
+      email: req.body.email,
+      password: req.body.password
+    }
+    User.create(input)
+      .then((data) => {
+        res.status(201).json({ id: data.id, email: data.email })
+      })
+      .catch((err) => {
+        next(err)
+      })
+  }
+
+  static login(req, res, next) {
+    let input = {
+      email: req.body.email,
+      password: req.body.password
+    }
+    User.findOne({ where: { email: input.email }})
+      .then ((data) => {
+        if (data){
+          let compare = comparepass(input.password, data.password)
+          if (compare){
+            let payload = { id: data.id, email: data.email }
+            let token = generateToken(payload)
+            res.status(200).json({ access_token: token })
+          } else {
+            throw { name: 'invalidlogin' }
+          }
+        } else {
+          throw { name: 'invalidlogin' }
+        }
+      })
+      .catch ((err) => {
+        next(err)
+      })
+  }
 }
 
 module.exports = { UserController }
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,6 @@
 const router = require('express').Router()
+const user = require('./user')
+
+router.use('/', user)
+
+module.exports = router
