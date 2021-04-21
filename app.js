1a1bacc1add81c6024133576942241acdde80be8
Mike <Mike@Legion>
2021-04-21 22:43:14 +0700

    Ready to deploy

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const express = require('express')
 const router = require('./routes')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
