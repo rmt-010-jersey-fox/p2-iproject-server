9b6bfe3b3abcef3640d3d1d98e58d990ce75c638
bobybewa <bobybewa6@gmail.com>
2021-04-22 02:25:38 +0700

    login, regis, and use json web token

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,6 @@ const Controller = require('../controllers/controller')
 router.get('/showCase', Controller.showCase)
 router.get('/globalCase', Controller.worldCaseShow)
 router.get('/news', Controller.news)
+router.post('/register', Controller.register)
+router.post('/login', Controller.login)
 module.exports = router
