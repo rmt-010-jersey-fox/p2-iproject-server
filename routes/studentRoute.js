73d0f6c776bd99f0fc1f0d049f70e2eedab8dcf5
ekhaer <emiliakhaer@gmail.com>
2021-04-21 11:56:12 +0700

    student /PUT cancel booking

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const studentController = require('../controllers/studentController');
 const authenticate = require('../middlewares/authentication')
+const authorization = require('../middlewares/auth-student')
 
 router.use(authenticate)
 
@@ -12,6 +13,7 @@ router.get('/materials/:id', studentController.getBuddyMaterialsById)
 router.post('/booking', studentController.booking)
 router.get('/schedule', studentController.schedule)
 
-
+router.use('/booking/:id', authorization)
+router.put('/booking/:id', studentController.cancelBooking)
 
 module.exports = router
