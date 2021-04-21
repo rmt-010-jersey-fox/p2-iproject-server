e367218ce6e9727cb1f131b7ba5c2f4c85005adc
Andi Javier <andijavier442@gmail.com>
2021-04-21 07:48:39 +0700

    heroku lancar

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV !== "production") {
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const router = require('./routes')
 const errHandler = require('./middlewares/errHandler')
 
