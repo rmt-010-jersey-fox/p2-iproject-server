9f0fa510ebc06abce675a0309e16940b27e29a95
ciniaeleonora <ciniadelamotte@gmail.com>
2021-04-21 23:39:25 +0800

    fixing search recipes

--- controllers/drinksController.js
+++ controllers/drinksController.js
@@ -67,18 +67,34 @@ class DrinkController{
 
 
     static searchDrinks(req, res, next){
-        let name = req.body.name
+        let name = req.params.name
         axios({
             method: 'post',
-            url: `http://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita${name}`,
+            url: `http://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`,
         })
-            .then(data => {
-                console.log(data.data);
-                res.status(200).json(data.data)
+        .then(response => {
+            let data = response.data.drinks[0]
+            console.log(data);
+            let id = data.idDrink
+            let name = data.strDrink
+            let category = data.strCategory
+            let alcoholic = data.strAlcoholic
+            let instructions = data.strInstructions
+            let imgUrl = data.strDrinkThumb
+            let ingridientsRaw = [ data.strIngredient1,  data.strIngredient2,  data.strIngredient3,  data.strIngredient4,  data.strIngredient5,  data.strIngredient6,  data.strIngredient7,  data.strIngredient8,  data.strIngredient9, data.strIngredient10, data.strIngredient11, data.strIngredient12, data.strIngredient13, data.strIngredient14, data.strIngredient15, data.strIngredient16, data.strIngredient17, data.strIngredient18, data.strIngredient19, data.strIngredient20]
+            let ingridients = []
+
+            ingridientsRaw.forEach(el => {
+                if (el){
+                    ingridients.push(el)
+                }
             })
-            .catch(err => {
-                console.log(err);
-            })
+            
+            res.status(200).json({id, name, category, alcoholic, instructions, imgUrl, ingridients})
+        })
+        .catch(err => {
+            console.log('error');
+        })
     }
 }
 
--- controllers/mealsController.js
+++ controllers/mealsController.js
@@ -61,14 +61,30 @@ class MealController{
     }
 
     static searchMeals(req, res, next){
-        let name = req.body.name
+        let name = req.params.name
         axios({
             method: 'post',
             url: `http://www.themealdb.com/api/json/v1/1/search.php?s=${name}`,
         })
-            .then(data => {
-                console.log(data.data);
-                res.status(200).json(data.data)
+            .then(response => {
+                // console.log(response.data);
+                let data = response.data.meals[0]
+                let id = data.idMeal
+                let name = data.strMeal
+                let category = data.strCategory
+                let area = data.strArea
+                let imgUrl = data.strMealThumb
+                let instructions = data.strInstructions
+                let ingridientsRaw = [`${data.strMeasure1} ${data.strIngredient1}`, `${data.strMeasure2} ${data.strIngredient2}`, `${data.strMeasure3} ${data.strIngredient3}`, `${data.strMeasure4} ${data.strIngredient4}`, `${data.strMeasure5} ${data.strIngredient5}`, `${data.strMeasure6} ${data.strIngredient6}`, `${data.strMeasure7} ${data.strIngredient7}`, `${data.strMeasure8} ${data.strIngredient8}`, `${data.strMeasure9} ${data.strIngredient9}`, `${data.strMeasure10} ${data.strIngredient10}`, `${data.strMeasure11} ${data.strIngredient11}`, `${data.strMeasure12} ${data.strIngredient12}`, `${data.strMeasure13} ${data.strIngredient13}`, `${data.strMeasure14} ${data.strIngredient14}`, `${data.strMeasure15} ${data.strIngredient15}`, `${data.strMeasure16} ${data.strIngredient16}`, `${data.strMeasure17} ${data.strIngredient17}`, `${data.strMeasure18} ${data.strIngredient18}`, `${data.strMeasure19} ${data.strIngredient19}`, `${data.strMeasure20} ${data.strIngredient20}`]
+                let ingridients = []
+                ingridientsRaw.forEach(el => {
+                    if (el.length > 2){
+                        ingridients.push(el)
+                    }
+                })
+
+                res.status(200).json({id, name, category, area, imgUrl, instructions, ingridients })
+                // res.status(200).json(response.data)
             })
             .catch(err => {
                 console.log(err);
