fbf8f2bc92f6ed623901445c569858ee8d54dbba
ciniaeleonora <ciniadelamotte@gmail.com>
2021-04-22 03:01:49 +0800

    set node cron

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const router = require('./routes')
 const cors = require('cors')
+const sendEmail = require('./helpers/nodeMailer')
 
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(router)
+app.use(sendEmail)
 
 app.listen(PORT, () => {
     console.log(`FoodLova app listening at http://localhost:${PORT}`)
--- helpers/nodeMailer.js
+++ helpers/nodeMailer.js
@@ -1,29 +1,115 @@
 const nodemailer = require("nodemailer")
+const axios = require('axios')
+const cron = require('node-cron');
+const { User } = require('../models')
 
-function sendEmail(user, email, message) {
-
-  //Step 1: Creating the transporter
-  const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    auth: {
-      user: "recipes.world22@gmail.com",
-      pass: "hacktiv8"
+function sendEmail () {
+  let users = []
+  let message = ''
+  
+  
+  axios({
+    method: 'post',
+    url: `https://www.themealdb.com/api/json/v1/1/random.php`,
+  })        
+    .then(response => {
+      let data = response.data.meals[0]
+      let name = data.strMeal
+      let instructions = data.strInstructions
+      let ingridientsRaw = [`${data.strMeasure1} ${data.strIngredient1}`, `${data.strMeasure2} ${data.strIngredient2}`, `${data.strMeasure3} ${data.strIngredient3}`, `${data.strMeasure4} ${data.strIngredient4}`, `${data.strMeasure5} ${data.strIngredient5}`, `${data.strMeasure6} ${data.strIngredient6}`, `${data.strMeasure7} ${data.strIngredient7}`, `${data.strMeasure8} ${data.strIngredient8}`, `${data.strMeasure9} ${data.strIngredient9}`, `${data.strMeasure10} ${data.strIngredient10}`, `${data.strMeasure11} ${data.strIngredient11}`, `${data.strMeasure12} ${data.strIngredient12}`, `${data.strMeasure13} ${data.strIngredient13}`, `${data.strMeasure14} ${data.strIngredient14}`, `${data.strMeasure15} ${data.strIngredient15}`, `${data.strMeasure16} ${data.strIngredient16}`, `${data.strMeasure17} ${data.strIngredient17}`, `${data.strMeasure18} ${data.strIngredient18}`, `${data.strMeasure19} ${data.strIngredient19}`, `${data.strMeasure20} ${data.strIngredient20}`]
+      let ingridients = []
+      ingridientsRaw.forEach(el => {
+      if (el.length > 2){
+        ingridients.push(el)
+        }
+      })
+  
+      const message = `
+      name: ${name},
+      category: ${category},
+      ingridients: ${ingridients}
+      instructions: ${instructions}
+      `
+      message = message
+    })
+      .catch(err => {
+        next(err)
+    })
+  
+  
+  User.findAll() 
+    .then(users => {
+      users = users.email
+    })
+  
+  
+  
+  users.forEach(user => {
+      //Step 1: Creating the transporter
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: "recipes.world22@gmail.com",
+        pass: "hacktiv8"
+      }
+   })
+  
+    //Step 2: Setting up message options
+    const messageOptions = {
+    from: "recipes.world22@gmail.com",
+    to: user.email,
+    subject: "Random Recipes from Recipes world",
+    text: `Hello ${user},
+   
+   
+    ${message}
+   
+    Thank you!`
     }
+  
+    //Step 3: Sending email
+    const task = cron.schedule('* * * * *', () => {
+      transporter.sendMail(messageOptions, (err, info) => {
+        if(err){
+          console.log(err)
+        } else {
+          console.log('email sent');
+        }
+      });
+    })
+  
   })
-
-  //Step 2: Setting up message options
-  const messageOptions = {
-  subject: "Random Recipes from Recipes world",
-  text: `Hello ${user},
-  
-  
-  ${message}`,
-  to: email,
-  from: "recipes.world22@gmail.com"
-  }
-
-  //Step 3: Sending email
-  transporter.sendMail(messageOptions);
 }
 
+
 module.exports = sendEmail
+
+
+
+
+// function sendEmail(message, email) {
+
+//   //Step 1: Creating the transporter
+//   const transporter = nodemailer.createTransport({
+//     service: "Gmail",
+//     auth: {
+//       user: "recipes.world22@gmail.com",
+//       pass: "hacktiv8"
+//     }
+//   })
+
+//   //Step 2: Setting up message options
+//   const messageOptions = {
+//   from: "recipes.world22@gmail.com",
+//   to: email,
+//   subject: "Random Recipes from Recipes world",
+//   text: `Hello ${user},
+  
+  
+//   ${message}
+  
+//   Thank you!`
+//   }
+// }
+
+
