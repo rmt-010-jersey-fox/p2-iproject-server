fbd0568aa4f7c13c28d71f4cb160e54ee9c46782
abdulmajid <abdulmajid08180881@gmail.com>
2021-04-22 05:53:57 +0700

    success showALL

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router()
-const UserController = require('../controllers/userControllers')
+// const UserController = require('../controllers/userControllers')
 const MovieController = require('../controllers/movieControllers')
-const authentication = require('../middlewares/authentication')
+// const authentication = require('../middlewares/authentication')
 
-router.post('/register', UserController.register)
-router.post('/login', UserController.login)
-
+// router.post('/register', UserController.register)
+// router.post('/login', UserController.login)
 // router.use(authentication)
+
 router.get('/movies/popular', MovieController.moviePopular)
 router.get('/movies/top_rated', MovieController.movieTopRate)
 router.get('/movies/upcoming', MovieController.moviesUpcoming)
