acb8e145b0ba543643947fe1c70c2a541be0281b
ciniaeleonora <ciniadelamotte@gmail.com>
2021-04-22 02:09:05 +0800

    add node mailer

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,44 @@
 const { User } = require('../models')
-const jwt = require('../helpers/jwt')
-const bcrypt = require('../helpers/bcrypt')
-
+const { comparePassword } = require('../helpers/bcrypt')
+const { generateToken } = require('../helpers/jwt')
+const sendMail = require('../helpers/nodeMailer')
+const axios = require('axios')
 
 class UserController {
     static register(req, res, next){
         let {name, email, password} = req.body
         User.create({name, email, password})
             .then(user => {
-                res.status(200).json(user)
+                return axios({
+                    method: 'post',
+                    url: `https://www.themealdb.com/api/json/v1/1/random.php`,
+                })
+            })
+        
+            .then(response => {
+                let data = response.data.meals[0]
+                let id = data.idMeal
+                let name = data.strMeal
+                let category = data.strCategory
+                let area = data.strArea
+                let imgUrl = data.strMealThumb
+                let instructions = data.strInstructions
+                let ingridientsRaw = [`${data.strMeasure1} ${data.strIngredient1}`, `${data.strMeasure2} ${data.strIngredient2}`, `${data.strMeasure3} ${data.strIngredient3}`, `${data.strMeasure4} ${data.strIngredient4}`, `${data.strMeasure5} ${data.strIngredient5}`, `${data.strMeasure6} ${data.strIngredient6}`, `${data.strMeasure7} ${data.strIngredient7}`, `${data.strMeasure8} ${data.strIngredient8}`, `${data.strMeasure9} ${data.strIngredient9}`, `${data.strMeasure10} ${data.strIngredient10}`, `${data.strMeasure11} ${data.strIngredient11}`, `${data.strMeasure12} ${data.strIngredient12}`, `${data.strMeasure13} ${data.strIngredient13}`, `${data.strMeasure14} ${data.strIngredient14}`, `${data.strMeasure15} ${data.strIngredient15}`, `${data.strMeasure16} ${data.strIngredient16}`, `${data.strMeasure17} ${data.strIngredient17}`, `${data.strMeasure18} ${data.strIngredient18}`, `${data.strMeasure19} ${data.strIngredient19}`, `${data.strMeasure20} ${data.strIngredient20}`]
+                let ingridients = []
+                ingridientsRaw.forEach(el => {
+                    if (el.length > 2){
+                        ingridients.push(el)
+                    }
+                })
+
+                const message = `
+                name: ${name},
+                category: ${category},
+                ingridients: ${ingridients}
+                instructions: ${instructions}
+                `
+                sendMail(name, email, message)
+                res.status(200).json({id, name, category, area, imgUrl, instructions, ingridients })
             })
             .catch(err => {
                 next(err)
@@ -24,8 +54,10 @@ class UserController {
                     next({name: 'invalid email or password'})
                     console.log('salah 1');
                 } else {
-                    const isPassMatched = comparePassword(password, user.password)
-                    if (!isPassMatched) {
+                    console.log(password)
+                    console.log(user.password)
+                    const isMatched = comparePassword(password, user.password)
+                    if (!isMatched) {
                         next({name: 'invalid email or password'})
                         console.log('salah 2');
                     } else {
@@ -44,7 +76,9 @@ class UserController {
                 }
             })
             .catch(err => {
-                console.log('salah 3');
+                console.log('salah 3')
+                console.log('----')
+                console.log(err)
                 next(err)
             })
     }
