58b88422813bb9f28f8597a463158b0a97d64a48
maulidyahintan <maulyintan88@gmail.com>
2021-04-22 13:16:51 +0700

    mail transaksi

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -5,13 +5,15 @@ class TransactionController {
     static create (req, res, next) {
         const CustomerId = req.currentUser.id
         const {CarId, start_date, end_date, location} = req.body
+        let obj;
         Transaction.create({CustomerId, CarId, start_date, end_date, location})
         .then(data => {
             // res.status(200).json(data)
-            sendMail(req.currentUser.email, obj)
+            obj = data
+            return sendMail(req.currentUser.email, obj)
             .then(result => {
                 console.log('Email sent .....', result);
-                res.status(201).json(data)
+                res.status(201).json(obj)
             })
         })
         .catch(err => {
