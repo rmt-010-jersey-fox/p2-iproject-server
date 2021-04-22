ac27fb1d6d40b8473309a1bf5f8c73f80e5bc805
bobybewa <bobybewa6@gmail.com>
2021-04-22 10:15:17 +0700

    setup deploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config()
 }
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const routes = require('./routes')
 const cors = require('cors')
 const httpServer = require("http").createServer(app);
