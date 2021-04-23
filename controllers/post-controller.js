a7c8bab8c58ab0547238cba2a433ac94d31bfd7e
Indra Akhmad <indraahmadfirdaus@gmail.com>
2021-04-23 09:52:03 +0700

    adding cloudinary service

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -2,6 +2,14 @@ const {
     Post
 } = require('../models')
 
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({ 
+    cloud_name: 'dcudqbeko', 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+  });
+
 class PostController {
     static async getPosts(req, res, next) {
         try {
@@ -46,15 +54,28 @@ class PostController {
             if(!req.body.caption) throw { status: 400, message: "Must Give a Caption"}
 
             req.file.path = req.file.path.replace('\\', '/')
-            const addPost = await Post.create({
-                filePath: req.file.path,
-                caption: req.body.caption,
-                UserId: req.loggedUser.id
+
+            cloudinary.uploader.upload(req.file.path, async (err, image) => {
+                try {
+                    if(!err) {
+                        const addPost = await Post.create({
+                            filePath: image.secure_url,
+                            caption: req.body.caption,
+                            UserId: req.loggedUser.id
+                        })
+    
+                        res.status(201).json({
+                            message: 'Upload Success',
+                            data: addPost
+                        })
+                    } else {
+                        throw { status:500, message: "Something on the cloud happened"}
+                    }
+                } catch (err) {
+                    next(err)
+                }
             })
-            res.status(201).json({
-                message: 'Upload Success',
-                data: addPost
-            })
+
         } catch (err) {
             next(err)
         }
